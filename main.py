c2fa484b11f26122e6591ee1360603e9a165ebf1
Nik <31962595+starscreamnik@users.noreply.github.com>
2019-09-30 14:43:05 +0200

    Parallel algorithm for 'response', fixed bandpass and barrage filter

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import math
 import random
+import threading
 
 random.seed()
 pi = np.pi
@@ -24,30 +25,30 @@ def polyharmonic1(t):
 
 def low_pass_filter(n, omega_c):
     if n == 0:
-        return omega_c / pi
+        return omega_c[0] / pi
     else:
-        return sin(omega_c * n) / (pi * n)
+        return sin(omega_c[0] * n) / (pi * n)
 
 
 def high_pass_filter(n, omega_c):
     if n == 0:
-        return 1 - omega_c / pi
+        return 1 - omega_c[0] / pi
     else:
-        return - sin(omega_c * n) / (pi * n)
+        return - sin(omega_c[0] * n) / (pi * n)
 
 
-def bandpass_filter(n, f1, f2, w1, w2):
+def bandpass_filter(n, omega_c):
     if n == 0:
-        return 2 * (f2 - f1)
+        return (omega_c[1] - omega_c[0]) / pi
     else:
-        return sin(w2 * n) / (pi * n) - sin(w1 * n) / (pi * n)
+        return sin(omega_c[1] * n) / (pi * n) - sin(omega_c[0] * n) / (pi * n)
 
 
-def barrage_filter(n, f1, f2, w1, w2):
+def barrage_filter(n, omega_c):
     if n == 0:
-        return 1 - 2 * (f2 - f1)
+        return 1 - (omega_c[1] - omega_c[0]) / pi
     else:
-        return sin(w1 * n) / (pi * n) - sin(w2 * n) / (pi * n)
+        return sin(omega_c[0] * n) / (pi * n) - sin(omega_c[1] * n) / (pi * n)
 
 
 def_filters = {
@@ -58,90 +59,107 @@ def_filters = {
 }
 
 
-def kaiser_window(omega_s, omega_p, omega_a, A_o, type="low"):
-    # omega_s - частота дискретизации
-    # omega_p - полоса пропускания
-    # omega_a - полоса заграждения
-    # A_o - параметр, определяющий A_a, A_p
+def kaiser_window(type):
+    N = 1 if type == "low" or type == "high" else 2
+
+    omega_c = []
+    order = 3
+    print("Kaiser Window:")
+    for i in range(N):
+        print(i + 1, "| omega_s:")
+        omega_s = float(input())  # omega_s - частота дискретизации
+        print(i + 1, "| omega_p:")
+        omega_p = float(input())  # omega_p - полоса пропускания
+        print(i + 1, "| omega_a:")
+        omega_a = float(input())  # omega_a - полоса заграждения
+        print(i + 1, "| A_o:")
+        A_o = float(input())  # A_o - параметр, определяющий A_a, A_p
+
+        delta1 = pow(10, -0.05 * A_o)
+        delta2 = (pow(10, 0.05 * A_o) - 1) / (pow(10, 0.05 * A_o) + 1)
+        delta = min(delta1, delta2)
+        A_a = -20 * lg(delta)  # Минимальное затухание в полосе задерживания
+
+        # Максимально допустимая пульсация в полосе пропускания
+        # A_p = 20 * lg((1 + delta) / (1 - delta))
+
+        omega_c.append((omega_p + omega_a) / 2)
+        D = 0.9222 if (A_a <= 21) else (A_a - 7.95) / 14.36
+        alpha = (0.0 if A_a <= 21 else
+                 0.5842 * pow(A_a - 21, 0.4) + 0.07886 * (A_a - 21) if A_a <= 50 else
+                 0.1102 * (A_a - 8.7))
+
+        order = ceil(omega_s * D / (omega_a - omega_p) * 0.5) * 2 + 1  # Длина фильтра
+
+    n = yield order, omega_c
+
+    while True:
+        beta = alpha * sqrt(1 - pow(2 * n / (order - 1), 2))
+        n = yield np.i0(beta) / np.i0(alpha)
+
+
+def hamming_window(type):
+    alpha = 0.54
+    N = 1 if type == "low" or type == "high" else 2
+    omega_c = []
+
+    print("Hamming Window:")
+    for i in range(N):
+        print(i, ": omega_c")
+        omega_c.append(float(input()))
+
+    print("order:")
+    order = int(input())
+
+    n = yield order, omega_c
+    while True:
+        n = yield alpha - (1 - alpha) * cos(2 * pi * n / (order - 1))
+
+
+def_windows = {
+    'kaiser': kaiser_window,
+    'hamming': hamming_window
+}
+
+
+def getFir(w_type, f_type):
     H = []  # Импульсная характеристика фильтра
     H_d = []  # Идеальная импульсная характеристика
     W = []  # Весовая функция
 
     try:
-        filter = def_filters[type]
+        filter = def_filters[f_type]
+        window = def_windows[w_type]
     except KeyError as e:
         raise ValueError('Undefined filter type: {}'.format(e.args[0]))
 
-    delta1 = pow(10, -0.05 * A_o)
-    delta2 = (pow(10, 0.05 * A_o) - 1) / (pow(10, 0.05 * A_o) + 1)
-    delta = min(delta1, delta2)
-    A_a = -20 * lg(delta)  # Минимальное затухание в полосе задерживания
-
-    # Максимально допустимая пульсация в полосе пропускания
-    # A_p = 20 * lg((1 + delta) / (1 - delta))
-
-    omega_c = (omega_p + omega_a) / 2
-    D = 0.9222 if (A_a <= 21) else (A_a - 7.95) / 14.36
-    alpha = (0.0 if A_a <= 21 else
-             0.5842 * pow(A_a - 21, 0.4) + 0.07886 * (A_a - 21) if A_a <= 50 else
-             0.1102 * (A_a - 8.7))
-
-    size = ceil(omega_s * D / (omega_a - omega_p) * 0.5) * 2 + 1  # Длина фильтра
-    start, fin = -floor(size / 2), floor(size / 2)
-    print("size, start, fin:", size, start, fin)
+    win_gen = window(f_type)  # Init a generator for selected window
+    order, omega_c = win_gen.send(None)  # First call: pre-calculation
+    start, fin = -floor(order / 2), floor(order / 2)
 
     for n in range(start, fin + 1):
         H_d.append(filter(n, omega_c))
         # весовая функция Кайзера
-        beta = alpha * sqrt(1 - pow(2 * n / (size - 1), 2))
-        # i0 - модифицированная функция Бесселя 1-го рода 0-го порядка
-        W.append(np.i0(beta) / np.i0(alpha))
+        W.append(win_gen.send(n))
         # Преобразование коэффициентов по принципу свертки
         H.append(H_d[-1] * W[-1])
 
+    win_gen.close()
+
     # Нормировка импульсной характеристики
     sum = 0.0
-    for i in range(size):
+    for i in range(order):
         sum += H[i]
-    for i in range(size):
+    for i in range(order):
         H[i] /= sum
     return H
 
 
-def hamming_window(size, alpha, Fc, type="low"):
-    if alpha < 0 or alpha > 1:
-        return False
-
-    H = []  # Импульсная характеристика фильтра
-    H_d = []  # Идеальная импульсная характеристика
-    W = []  # Весовая функция
-
-    try:
-        filter = def_filters[type]
-    except KeyError as e:
-        raise ValueError('Undefined filter type: {}'.format(e.args[0]))
-
-    start, fin = -floor(size / 2), floor(size / 2)
-
-    print("size, start, fin:", size, start, fin)
-
-    for n in range(start, fin + 1):
-        H_d.append(filter(n, Fc))
-        # весовая функция Кайзера
-        W.append(alpha - (1 - alpha) * cos(2 * pi * n / (size - 1)))
-        # Преобразование коэффициентов по принципу свертки
-        H.append(H_d[-1] * W[-1])
-
-    # Нормировка импульсной характеристики
-    sum = 0.0
-    for i in range(size):
-        sum += H[i]
-    for i in range(size):
-        H[i] /= sum
-    return H
+def worker(output, H, input):
+    output += H * input
 
 
-def response(input, H):
+def response(input, H, isParallel=False):
     if len(input) < 1:
         return 0
 
@@ -151,23 +169,19 @@ def response(input, H):
 
     for i in range(firSize):
         if inpSize - 1 - i >= 0:
-            output += H[i] * input[-1 - i]
+            if isParallel:
+                t = threading.Thread(target=worker, args=(output, H[i], input[-1 - i]))
+                t.start()
+            else:
+                output += H[i] * input[-1 - i]
+
+    if isParallel:
+        main_thread = threading.currentThread()
+        for t in threading.enumerate():
+            if t is main_thread:
+                continue
+            t.join()
     return output
-
-
-H1 = kaiser_window(1, 0.15, 0.3, 30, "low")
-H2 = hamming_window(5, 0.54, 0.001, "low")
-t = list(range(200))
-input = []
-output1 = []
-output2 = []
-
-for i in t:
-    input.append(polyharmonic1(i))
-    # if i >= len(H1):
-    output1.append(response(input, H1))
-    # if i >= len(H2):
-    output2.append(response(input, H2))
 
 
 def plotGraphic(func, delta_y, name="Function"):
@@ -179,8 +193,8 @@ def plotGraphic(func, delta_y, name="Function"):
     plt.show()
 
 
-def plotFFTGraphic(N, T, y, name="FFT Function"):
-    x = np.linspace(0.0, N * T, N)
+def plotFFTGraphic(y, T, name="FFT Function"):
+    N = len(y)
     yf = fft(y)
     xf = np.linspace(0.0, 1.0 / (2.0 * T), N // 2)
     plt.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]))
@@ -189,10 +203,47 @@ def plotFFTGraphic(N, T, y, name="FFT Function"):
     plt.show()
 
 
-plotGraphic(input, 35, "Input")
-plotGraphic(output1, 35, "Output After Kaiser")
-plotGraphic(output2, 35, "Output After Hamming")
+def sequence(t, H1, H2):
+    input = []
+    output1 = [0.] * t
+    output2 = [0.] * t
 
-plotFFTGraphic(len(input), 1, input, "FFT Input")
-plotFFTGraphic(len(output1), 1, output1, "FFT Output After Kaiser")
-plotFFTGraphic(len(output2), 1, output2, "FFT Output After Hamming")
+    for i in range(t):
+        input.append(polyharmonic1(i))
+        output1[i] = response(input, H1)
+        output2[i] = response(input, H2)
+
+    return input, output1, output2
+
+
+def parallel(t, H1, H2):
+    input = []
+    output1 = []
+    output2 = []
+
+    for i in range(t):
+        input.append(polyharmonic1(i))
+        output1.append(response(input, H1))
+        output2.append(response(input, H2))
+
+    return input, output1, output2
+
+
+def main():
+    H1 = getFir("kaiser", "band")
+    H2 = getFir("hamming", "band")
+
+    # input, output1, output2 = sequence(200, H1, H2)
+    input, output1, output2 = parallel(200, H1, H2)
+
+    plotGraphic(input, 35, "Input")
+    plotGraphic(output1, 35, "Output After Kaiser")
+    plotGraphic(output2, 35, "Output After Hamming")
+
+    plotFFTGraphic(input, 1, "FFT Input")
+    plotFFTGraphic(output1, 1, "FFT Output After Kaiser")
+    plotFFTGraphic(output2, 1, "FFT Output After Hamming")
+
+
+if __name__ == "__main__":
+    main()
